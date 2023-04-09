de88d65690a4156602209c2c0f78e0f360c2b6ca
Michael Loughman <mcloughman@gmail.com>
2023-04-08 21:19:27 -0700

    Install and implement jsonwebtoken in our userControllers file. Test it in Postman.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,14 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 // login
 const loginUser = async (req, res) => {
   res.json({ mssg: "login user" });
+};
+
+// we create this function beacuse we need it in both login and signup
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
 // signup
@@ -11,8 +17,10 @@ const signupUser = async (req, res) => {
   try {
     // .signup is the static method we added to user model
     const user = await User.signup(email, password);
+    //create token
+    const token = await createToken(user._id);
     // usere being the new document that was created in mogodb
-    res.status(200).json({ email, user });
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
