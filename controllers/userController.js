83239108a3658c395c81706ee8f40e1c4e9534e2
Michael Loughman <mcloughman@gmail.com>
2023-04-08 18:44:48 -0700

    Create a static method in our userModel file. Install bcrypt and use it's methods to salt and hash passwords when user signs up. Create the code for signup route in controllers. Test for functionallity in Postman.

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,15 @@ const loginUser = async (req, res) => {
 
 // signup
 const signupUser = async (req, res) => {
-  res.json({ mssg: "signup user" });
+  const { email, password } = req.body;
+  try {
+    // .signup is the static method we added to user model
+    const user = await User.signup(email, password);
+    // usere being the new document that was created in mogodb
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { signupUser, loginUser };
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,20 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+// We're going to make a static method on our user model
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // can't use arrow function because we need the keyword this
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
