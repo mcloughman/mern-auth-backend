9e80d62c8a81ce46f82c2666cabb88cb40e22dcf
Michael Loughman <mcloughman@gmail.com>
2023-04-26 19:52:47 -0700

    add line to server.js to hopefully eliminate CORS issue when signing up or loggin in

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
